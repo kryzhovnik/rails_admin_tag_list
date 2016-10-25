00d20e975b0e5b85928ea7eda81f11f902081cdc
Andrey Samsonov <andrey.samsonov@gmail.com>
2016-10-25 23:09:54 +0400

    Include view helper module on initialization

--- lib/rails_admin_tag_list/engine.rb
+++ lib/rails_admin_tag_list/engine.rb
@@ -1,4 +1,9 @@
 module RailsAdminTagList
   class Engine < ::Rails::Engine
+    initializer 'rails_admin_rag_list.action_controller' do |app|
+      ActiveSupport.on_load :action_controller do
+        helper RailsAdminTagList::SuggestionsHelper
+      end
+    end
   end
 end
